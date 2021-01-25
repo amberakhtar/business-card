f66dbb884f7d385f0054216ea7d48b2ccf4bbe1d
Amber <amber_ak@live.com>
2021-01-25 23:09:19 +0500

    Initial Commit

---
+++ AmberCard/AmberCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmberCardApp.swift
+//  AmberCard
+//
+//  Created by Amber Akhtar on 1/25/21.
+//
+
+import SwiftUI
+
+@main
+struct AmberCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
